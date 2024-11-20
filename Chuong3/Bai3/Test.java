8540228e36b188c94b4aca945fc094e934270c68
khuong1742005 <boyzsno1@gmail.com>
2024-11-20 21:26:06 +0700

    add bai 3 chuong 3;

---
+++ Chuong3/Bai3/Test.java
@@ -0,0 +1,10 @@
+package Chuong3.Bai3;
+
+public class Test {
+    public static void main(String[] args) {
+        NhanVien n = new NhanVien("Khuong", new DiaChi("Khoi 3", "Thi Tran", "Dien Chau", "Nghe An"), "17/04/2005", 'M');
+        System.out.println(n);
+    }
+
+
+}

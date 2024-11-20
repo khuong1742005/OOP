b78329051209dffda0e58469fd041fa21f58a689
khuong1742005 <boyzsno1@gmail.com>
2024-11-20 21:05:12 +0700

    add bai 2 chuong 3

---
+++ Chuong3/Bai2/Test.java
@@ -0,0 +1,9 @@
+package Chuong3.Bai2;
+
+public class Test {
+    public static void main(String[] args) {
+        DiaChi d = new DiaChi("Khoi 3", "Thi Tran", "Dien Chau", "Nghe An");
+        System.out.println(d);
+    }
+
+}
